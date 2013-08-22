7be69db851ac8c92303739e5539d51cb493d80f6
Gianluca Jellyman <mobile@jellyman.it>
2013-08-22 04:04:38 +0200

    7

--- www/cordova-2.3.0.js
+++ www/cordova-2.3.0.js
@@ -1,26 +1,3 @@
-// commit 4082ee0ba3a8902cba7bd087d0584bb7534e694b
-
-// File generated at :: Wed Jan 02 2013 16:43:38 GMT-0800 (PST)
-
-/*
- Licensed to the Apache Software Foundation (ASF) under one
- or more contributor license agreements.  See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership.  The ASF licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
- 
-     http://www.apache.org/licenses/LICENSE-2.0
- 
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
-*/
-
 ;(function() {
 
 // file: lib/scripts/require.js
--- www/js/index.js
+++ www/js/index.js
@@ -2,28 +2,55 @@ var app = {
     // Application Constructor
     initialize: function() {this.bindEvents();},
     // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    // Bind any events that are required on startup. Common events are: 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function ()
+    {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('offline', this.onOffline, false);
+        document.addEventListener('online', this.onOnline, false);
+        document.addEventListener('load', this.onLoad, false);
     },
+    ElShow: function (id) { document.getElementById(id).setAttribute('style', 'display:block;'); },
+    ElHide: function (id) { document.getElementById(id).setAttribute('style', 'display:none;'); },
+    /*
+    HideDiv: function (id)
+    {
+        document.getElementById('DeviceConnecting').setAttribute('style', 'display:none;');
+        document.getElementById('DeviceReady').setAttribute('style', 'display:none;');
+        document.getElementById('').setAttribute('style', 'display:none;');
+        document.getElementById('').setAttribute('style', 'display:none;');
+        document.getElementById(id).setAttribute('style', 'display:block;');
+    },
+    */
     // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+    // The scope of 'this' is the event. In order to call the 'DeviceReadyEvent' function, we must explicity call 'app.DeviceReadyEvent(...);'
+    onDeviceReady: function () { app.DeviceReadyEvent('DeviceReady'); },
+    DeviceReadyEvent: function (id)
+    {
+        app.ElHide('DeviceConnecting');
+        app.ElShow('DeviceReady');
+        console.log('Received Event: ' + id);
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    onOffline: function () { app.OfflineEvent('offline'); },
+    OfflineEvent: function (id)
+    {
+        app.ElHide('DeviceOnline');
+        app.ElShow('DeviceOffline');
+        console.log('Received Event: ' + id);
+    },
 
+    onOnline: function () { app.OnlineEvent('online'); },
+    OnlineEvent: function (id)
+    {
+        app.ElHide('DeviceOffline');
+        app.ElShow('DeviceOnline');
+        console.log('Received Event: ' + id);
+        //DeviceReadyElement.nodeValue = "Ciao"
+    },
+    onLoad: function () { app.LoadEvent('load'); },
+    LoadEvent: function (id)
+    {
         console.log('Received Event: ' + id);
     }
 };
